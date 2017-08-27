0505465415c718f396c02960cadf363c49279e50
k tm <kthumin@gmail.com>
2017-08-27 20:51:24 +0800

    Added Maven Wrapper for easy import and packaging

--- src/main/java/ktm/sp/test/web/swagger/SwaggerConfig.java
+++ src/main/java/ktm/sp/test/web/swagger/SwaggerConfig.java
@@ -2,7 +2,9 @@ package ktm.sp.test.web.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -20,6 +22,18 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select().apis(RequestHandlerSelectors.basePackage("ktm.sp.test.web"))
                 .paths(regex("/*.*"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Friend and Subscription Mangement")
+                .description("Test from SP")
+                .termsOfServiceUrl("http://springfox.io")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://github.com/kthumin/SPTest")
+                .version("1.0.0")
                 .build();
     }
 
